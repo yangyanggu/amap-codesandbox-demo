19c40330efbadf7b6eb2d6a1911ed4e97b2f0003
yangyang.gu <yangyang.gu@fjdynamics.com>
2022-12-09 09:43:27 +0800

    修改地图加载

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="main-container"></div>
+  <div class="main-container">
+    <el-amap />
+  </div>
 </template>
 
 <script setup lang="ts">
+import {ElAmap} from '@vuemap/vue-amap'
 </script>
 
 <style scoped>
 .main-container{
-
+  width: 100vw;
+  height: 100vh;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import '@vuemap/vue-amap/dist/style.css'
+import {initAMapApiLoader} from "@vuemap/vue-amap";
+initAMapApiLoader({
+    key: "8ebf373a63c7ea74828ea31d41917625",
+    securityJsCode: "ba47b8bba3e638feef8ae6941f1c32b5"
+})
 
 createApp(App).mount('#app')
